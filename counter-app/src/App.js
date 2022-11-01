964f409d17eb790066e1dd3cff1a41bc81754af8
aatbip <anantabipal2010@gmail.com>
2022-11-01 19:05:17 +0545

    improvement in notes

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -10,7 +10,7 @@ import DisplayNumber from "./components/DisplayNumber";
  * There is a useState hook in react for simple state management.
  */
 const App = () => {
-  //pass initial state to useState ie 0. useState returns variable (number) and function (setNumber)
+  //pass initial state to useState ie 0. useState returns variable (in our case -> number) and function (in our case -> setNumber)
   const [number, setNumber] = React.useState(0); //initial state
 
   // console.log(number); //number is a state variable variable has the current state
@@ -23,7 +23,7 @@ const App = () => {
   // console.log("second state", number);
 
   const increment = () => {
-    //setNumber function return the current state
+    //setNumber function accepts a callback that returns the current state
     // whatever setNumber returns, that will be the current state value
     setNumber((previous) => {
       return previous + 1;
